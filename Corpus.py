c92715f46326c83db9b1c71536446a9f2f96781d
Florian VACHER <vacherflorian43@gmail.com>
2021-12-28 17:00:12 +0100

    Graphe en place

--- Corpus.py
+++ Corpus.py
@@ -1,6 +1,10 @@
 import re
 from datetime import datetime
-import pandas
+# import pandas
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout 
+from stop_words import get_stop_words
 
 class Corpus:
     # nom -> le nom du corpus
@@ -31,12 +35,65 @@ class Corpus:
         tousDocuments.sort(key=lambda x: datetime.strptime(x.date, '%d/%m/%Y'))
         return tousDocuments
 
-    def concorde(self, mot, texteGlobal, nbAvAp):
-        df = pandas.DataFrame(columns=["Contexte gauche", "Texte", "Contexte droit"])
-        searches = re.finditer(mot, texteGlobal)
-        index = 0
-        for search in searches:
-            df.loc[index] = ["..." + texteGlobal[search.start() - nbAvAp : search.start() + 1], mot, texteGlobal[search.end() : search.end() + nbAvAp] + "..."]
-            # df.append(texteGlobal[search.start() - 10 : search.start()], mot, texteGlobal[search.end() : search.end() + 10])
-            index += 1
-        print(df)
+# =============================================================================
+#     def concorde(self, mot, texteGlobal, nbAvAp):
+#         df = pandas.DataFrame(columns=["Contexte gauche", "Texte", "Contexte droit"])
+#         searches = re.finditer(mot, texteGlobal)
+#         index = 0
+#         for search in searches:
+#             df.loc[index] = ["..." + texteGlobal[search.start() - nbAvAp : search.start() + 1], mot, texteGlobal[search.end() : search.end() + nbAvAp] + "..."]
+#             # df.append(texteGlobal[search.start() - 10 : search.start()], mot, texteGlobal[search.end() : search.end() + 10])
+#             index += 1
+#         print(df)
+# =============================================================================
+        
+    def creationGraphe(self, docs):
+        stop_words = get_stop_words('en')
+        stop_words = stop_words + ['robotic', 'robotics']
+        G = nx.Graph()
+        for doc in docs:
+            # Suppression des points dans le texte
+            txt = doc.texte.replace(".","")
+            txtSplit = txt.split()
+            txtClean = []
+            for indice in range(len(txtSplit)):
+                try:
+                    ind = stop_words.index(txtSplit[indice].lower())
+                except ValueError:
+                    ind = -1
+                # Suppression des "stop-words"
+                if(ind == -1):
+                    txtClean.append(txtSplit[indice])
+            if(len(txtClean) != 0):
+                for i in range(len(txtClean)):
+                    for j in range(i,len(txtClean)):
+                        if(txtClean[i].lower() != txtClean[j].lower()):
+                            try:
+                                G[txtClean[i].lower()][txtClean[j].lower()]['weight'] += 1
+                            except KeyError:     
+                                G.add_edge(txtClean[i].lower(), txtClean[j].lower(), weight=1)
+# =============================================================================
+#         pos = graphviz_layout(G, prog='dot')
+#         edge_labels = nx.get_edge_attributes(G, 'label')
+#         nx.draw(G, pos)
+#         nx.draw_networkx_edge_labels(G, pos, edge_labels, font_size=8)
+#         nx.draw_networkx_labels(G, pos, font_size=10)
+#         plt.show()
+# =============================================================================
+        aretes = list(G.edges(data='weight'))
+        for mot1,mot2,poids in aretes:
+            if poids <= 30:
+                G.remove_edge(mot1, mot2)
+        noeuds = list(G.nodes)
+        for noeud in noeuds:
+            if G.degree[noeud] < 1:
+                G.remove_node(noeud)
+        pos=nx.spring_layout(G)
+        nx.draw(G,pos)
+        # specifiy edge labels explicitly
+        edge_labels=dict([((u,v,),d['weight'])
+        for u,v,d in G.edges(data=True)])
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+        nx.draw_networkx_labels(G, pos, font_size=10)
+        # show graphs
+        plt.show()
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pickle
 from datetime import datetime
 import praw
 import re
-import pandas
+# import pandas
 
 # Dictionnaire ayant comme clé un id et comme valeur le titre du document
 id2doc = {}
@@ -48,18 +48,20 @@ def ajouterRedditDocument(document):
 
 def ajouterArxivDocument(document, auteurs):
     ajouterDoc(document)
-    for auteur in auteurs:
-        print(auteur)
-        if(auteur):
-            indexAuteur = auteurExiste(auteur['name'])
-            if indexAuteur == -1:
-                authors[len(authors)] = Auteur(auteur['name'], 1, [document])
-                if nature == 'Arxiv':
-                    document.addCoAuteur(auteur)
-                id2aut[len(authors) - 1] = auteur['name']
-            else:
-                authors[indexAuteur].ndoc += 1
-                authors[indexAuteur].add(document)
+# =============================================================================
+#     for auteur in auteurs:
+#         if(auteur):
+#             indexAuteur = auteurExiste(auteur['name'])
+#             print(indexAuteur)
+#             if indexAuteur == -1:
+#                 authors[len(authors)] = Auteur(auteur['name'], 1, [document])
+#                 if nature == 'Arxiv':
+#                     document.addCoAuteur(auteur)
+#                 id2aut[len(authors) - 1] = auteur['name']
+#             else:
+#                 authors[indexAuteur].ndoc += 1
+#                 authors[indexAuteur].add(document)
+# =============================================================================
 
 def conversionDate(date):
     dt = parse(date)
@@ -70,14 +72,14 @@ theme = "robotics"
 # Reddit connexion
 reddit = praw.Reddit(client_id='xFYa8Z8tZRG71TFbhGROLQ', client_secret='tR-4GMsMJNZx6wzQFGYYAp5ClUl0Ag', user_agent='WebScrapping_TP_1_2')
 
-nbResultReddit = 5
+nbResultReddit = 1
 
 docs = []
 top_posts = reddit.subreddit(theme).top('all',limit=nbResultReddit)
 for post in top_posts:
     docs.append(('Reddit', post))
 
-nbResultArxiv = 10
+nbResultArxiv = 6
 
 url = 'http://export.arxiv.org/api/query?search_query=all:' + theme + '&start=0&max_results=' + str(nbResultArxiv)
 data = urllib.request.urlopen(url)
@@ -99,7 +101,9 @@ for nature, doc in docs:
         docTmp = ArxivDocument(doc['title'], conversionDate(doc['published']), doc['id'], doc['summary'])
         ajouterArxivDocument(docTmp, doc['author'])
 
-print("Id2Doc : " + str(id2doc) + "\n")
+# =============================================================================
+# print("Id2Doc : " + str(id2doc) + "\n")
+# =============================================================================
 
 def getTexteGlobal():
     texteGlobal = ""
@@ -113,11 +117,13 @@ def getTexteGlobal():
 #     print(type(doc))
 #     print(str(doc) + "\n")
 
-print("Authors : ")
-for i in range(len(authors)):
-    print(str(authors[i]) + "\n")
-
-print("Id2aut : " + str(id2aut) + "\n")
+# =============================================================================
+# print("Authors : ")
+# for i in range(len(authors)):
+#     print(str(authors[i]) + "\n")
+# 
+# print("Id2aut : " + str(id2aut) + "\n")
+# =============================================================================
 
 corp = Corpus('Corpus 1', authors, id2aut, id2doc)
 # for document in corp.afficherDocuments(100):
@@ -140,15 +146,18 @@ corp = Corpus('Corpus 1', authors, id2aut, id2doc)
 #     print(repr(document))
 
 # Récupération de tous les textes dans une seule chaine de caractère
-texteGlobal = getTexteGlobal()
-t = re.finditer("effect", texteGlobal)
-print(t)
-for t_bis in t:
-    print(t_bis.end())
+# =============================================================================
+# texteGlobal = getTexteGlobal()
+# t = re.finditer("effect", texteGlobal)
+# print(t)
+# for t_bis in t:
+#     print(t_bis.end())
+# =============================================================================
 # print(f)
 
-corp.concorde("to", texteGlobal, 20)
+# corp.concorde("to", texteGlobal, 20)
 
-type, doc = docs[6]
-print("Texte :")
-print(doc)
+# =============================================================================
+# print(collection)
+# =============================================================================
+corp.creationGraphe(collection)
