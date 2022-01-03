18fd47f0f2eb07fd8080611e3995f1863579e6f9
Robin Ponson <rponson2001@gmail.com>
2022-01-03 17:52:36 +0100

    Rajout du fichier avec la gestion des valeurs du formulaire

---
+++ test_fenetre.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 11:31:46 2022
+
+@author: rpons
+"""
+
+from tkinter import *    
+fenetre = Tk()
+label = Label(fenetre, text="Hello World")
+label.pack()
+def recup_valeurs():
+    global mot 
+    mot = entree.get()  
+    global valeur
+    valeur=s.get()
+entree = Entry(fenetre, width=30)
+entree.pack()
+s = Spinbox(fenetre, from_=0, to=10)
+s.pack()
+bouton=Button(fenetre, text="Valider", command=recup_valeurs)
+bouton.pack()
+fenetre.mainloop()
+print(mot)
+print(valeur)
